1cfd3382e4121684f76956634d43da1a81abdac1
gnegash <143354308+gnegash@users.noreply.github.com>
2023-11-13 15:04:44 +0100

    Add files via upload

---
+++ SQLÖvningarGeweria/GeweriaIntro.sql
@@ -0,0 +1,24 @@
+--CREATE DATABASE KlassenDb;
+
+CREATE TABLE PeopleTbl(
+Id int,
+Förnamn varchar(255),
+Efternamn varchar(255),
+Ålder int,
+Hemort varchar(255),
+);
+
+INSERT INTO PeopleTbl(Id, Förnamn, Efternamn, Ålder, Hemort)
+VALUES 
+(1, 'Geweria', 'Negash', 20, 'Göteborg'),
+(2, 'Rasmus', 'Olofsson', 28, 'Kungälv'),
+(3, 'Eva-Lotta', 'Karlsson', 31, 'Göteborg'),
+(4, 'Anders', 'Öberg', 32, 'Frölunda'),
+
+
+
+
+
+
+
+
